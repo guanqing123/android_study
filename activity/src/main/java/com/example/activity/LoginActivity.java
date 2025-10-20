812370111564aa29e77e80db092a7a85a14a32fa
guanqing <guanqinghz@163.com>
2025-10-20 22:53:46 +0800

    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);

--- activity/src/main/java/com/example/activity/LoginActivity.java
+++ activity/src/main/java/com/example/activity/LoginActivity.java
@@ -15,6 +15,7 @@ public class LoginActivity extends AppCompatActivity {
         findViewById(R.id.btn_login).setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setClass(LoginActivity.this, LoginSuccessActivity.class);
+            // 设置启动标志：跳转到新的页面时，栈中的原有实例都被清空，同时开辟新任务的活动栈
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         });
