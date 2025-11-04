c5428a12b32bb8c80390eac9353ca6fd64dabe24
guanqing <guanqinghz@163.com>
2025-11-04 11:44:04 +0800

    移除RoomWriteActivity,转RoomProject

--- datastore/src/main/java/com/example/datastore/AppWriteActivity.java
+++ datastore/src/main/java/com/example/datastore/AppWriteActivity.java
@@ -1,13 +1,11 @@
 package com.example.datastore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-import java.util.Objects;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AppWriteActivity extends AppCompatActivity implements View.OnClickListener {
 
