43687eccb9ae2880f8202928d8611ef44d7f3210
guanqing <guanqinghz@163.com>
2025-11-18 08:28:15 +0800

    Fragment 动态注册

--- highcontrols/src/main/java/com/example/highcontrols/adapter/MobilePagerAdapter.java
+++ highcontrols/src/main/java/com/example/highcontrols/adapter/MobilePagerAdapter.java
@@ -16,6 +16,8 @@ public class MobilePagerAdapter extends FragmentPagerAdapter {
     private final List<GoodsInfo> goodsList;
 
     public MobilePagerAdapter(@NonNull FragmentManager fm, List<GoodsInfo> goodsList) {
+        // 会将当前fragment设置为Resume的状态，把上个fragment设置成Start的状态。
+        // 从而可以通过fragment的onResume()来懒加载数据。
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
         this.goodsList = goodsList;
     }
